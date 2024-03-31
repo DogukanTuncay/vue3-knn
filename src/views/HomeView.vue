3cfe7d31abc979fdaf7f451a44957fe7cb0c5033
Doğukan TUNÇAY <dogukantuncay1@gmail.com>
2024-03-31 14:18:27 +0300

    Another bug fixed again

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -105,18 +105,19 @@
 </div>
 
   </div>
+  <footer class="bottom-0 left-0 z-20 w-full mt-20 p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-center md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; Bu site <a href="https://www.linkedin.com/in/do%C4%9Fukan-tun%C3%A7ay-b58a8a225/" target="_blank" class="underline">Doğukan TUNÇAY</a> tarafından <strong>Antalya Belek Üniversitesi Yapay Zeka dersi  KNN Algoritması çözümü</strong>  için örnek olarak yapılmıştır. </span>
+    <div>
+
+    </div>
+</footer>
   </div>
 
 
 
   
 
-<footer class="fixed bottom-0 left-0 z-20 w-full mt-20 p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-center md:p-6 dark:bg-gray-800 dark:border-gray-600">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; Bu site <a href="https://www.linkedin.com/in/do%C4%9Fukan-tun%C3%A7ay-b58a8a225/" target="_blank" class="underline">Doğukan TUNÇAY</a> tarafından <strong>Antalya Belek Üniversitesi Yapay Zeka dersi  KNN Algoritması çözümü</strong>  için örnek olarak yapılmıştır. </span>
-    <div>
 
-    </div>
-</footer>
 </template>
 
 <script setup>
